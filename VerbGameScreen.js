94db5d8bcfc15138008878f98e17d9e6c8ff135b
Matan Gover <matangover@gmail.com>
2016-01-05 00:02:07 +0200

    Add back button from verb game to main screen

--- VerbGameScreen.js
+++ VerbGameScreen.js
@@ -1,30 +1,48 @@
 'use strict';
 
 var React = require('react-native');
-var {
-  Image,
-  StyleSheet,
-  Text,
-  View,
-} = React;
+var {StyleSheet, Text, TouchableHighlight, View} = React;
 
 var VerbGameScreen = React.createClass({
-  render: function() {
-    return (
-        <View style={styles.container}>
-            <Text>Verbs! Verbing? Has been being verbed.</Text>
-        </View>
-    );
-  },
+    render: function() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.content}>
+                    <Text>Verbs! Verbing? Has been being verbed.</Text>
+                </View>
+                <TouchableHighlight
+                    style={styles.backButton}
+                    onPress={() => this.props.navigator.pop()}>
+                    <Text style={styles.backText}>מסך ראשי</Text>
+                </TouchableHighlight>
+            </View>
+        );
+    }
 });
 
 var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#f5fcff'
+    },
+    content: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    backButton: {
+        alignSelf: 'center',
+        marginBottom: 10,
+        borderRadius: 4,
+        borderWidth: 2,
+        borderColor: '#7a9e2b'
+    },
+    backText: {
+        backgroundColor: '#a9d947',
+        color: 'white',
+        padding: 5,
+        fontWeight: 'bold'
+    }
 });
 
 module.exports = VerbGameScreen;

df86cc7f47c9ccc06d0fcf8a28ec85f76d19cf81
Matan Gover <matangover@gmail.com>
2016-01-23 04:42:21 +0200

    Refactor game into subcomponents.

--- VerbGameScreen.js
+++ VerbGameScreen.js
@@ -2,6 +2,7 @@
 
 var React = require('react-native');
 var {
+    Animated,
     PanResponder,
     StyleSheet,
     Text,
@@ -11,207 +12,205 @@ var {
 } = React;
 
 var CIRCLE_SIZE = 80;
-var CIRCLE_COLOR = 'blue';
-var CIRCLE_HIGHLIGHT_COLOR = 'green';
-
 
 var VerbGameScreen = React.createClass({
+    render: function() {
+        return (
+            <DragAndDropMenu>
+                <DragAndDropSubmenu title="עבר" style={{top:100, left: 100}}>
+                    <DragAndDropMenuItem />
+                    <DragAndDropMenuItem />
+                    <DragAndDropMenuItem />
+                </DragAndDropSubmenu>
+                <DragAndDropSubmenu title="ציווי" style={{top:200, left: 200}}>
+                    <DragAndDropMenuItem />
+                    <DragAndDropMenuItem />
+                    <DragAndDropMenuItem />
+                </DragAndDropSubmenu>
+                <DragAndDropSubmenu title="הווה/עתיד" style={{top:300, left: 300}}>
+                    <DragAndDropMenuItem />
+                    <DragAndDropMenuItem />
+                    <DragAndDropMenuItem />
+                </DragAndDropSubmenu>
+            </DragAndDropMenu>
+        )
+    },
+});
 
+var DragAndDropMenu = React.createClass({
     _panResponder: {},
-    _previousLeft: 0,
-    _previousTop: 0,
-    _circleStyles: {},
-    circle: (null : ?{ setNativeProps(props: Object): void }),
-    _hightlightedCircle: null,
 
     getInitialState: function() {
         return {
-            showSubmenu: false
+            activeSubmenu: null,
+            dragging: false,
+            pan: new Animated.ValueXY()
         };
     },
 
     componentWillMount: function() {
       this._panResponder = PanResponder.create({
         onStartShouldSetPanResponder: (evt, gestureState) => true,
+        onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
         onMoveShouldSetPanResponder: (evt, gestureState) => true,
-        onPanResponderGrant: (evt, gestureState) => this._highlight(),
+        onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
+        onPanResponderGrant: this._handlePanResponderGrant,
         onPanResponderMove: this._handlePanResponderMove,
         onPanResponderRelease: this._handlePanResponderEnd,
         onPanResponderTerminate: this._handlePanResponderEnd,
       });
-      this._previousLeft = 20;
-      this._previousTop = 84;
-      this._circleStyles = {
-        style: {
-          left: this._previousLeft,
-          top: this._previousTop
-        }
-      };
-    },
+      this.state.pan.addListener(value => {
+         this._currentPan = value;
+      });
 
-    componentDidMount: function() {
-        this._updatePosition();
+      this._childLayouts = [];
     },
-
 
     render: function() {
-        // return (
-        //     <View style={styles.container}>
-        //         <View style={styles.content}>
-        //             <Text>Verbs! Verbing? Has been being verbed.</Text>
-        //         </View>
-        //         <TouchableHighlight
-        //             style={styles.backButton}
-        //             onPress={() => this.props.navigator.pop()}>
-        //             <Text style={styles.backText}>מסך ראשי</Text>
-        //         </TouchableHighlight>
-        //     </View>
-        // );
-
-        var submenu = this.state.showSubmenu ?
-            <View style={[{left:100, top:300}, styles.genericCircle]} /> :
-                null;
-
         return (
             <View style={styles.container}>
-                <View
-                    ref={(circle) => {this.circle = circle;}}
-                    style={styles.circle}
+                <Animated.View
+                    style={[
+                        styles.circle,
+                        this.state.dragging && styles.draggingCircle,
+                        {transform: this.state.pan.getTranslateTransform()}]}
                     {...this._panResponder.panHandlers}
+                    onLayout={this._onLayout}
                 />
-                <View
-                    ref={(circle) => {this.circle1 = circle;}}
-                    style={[{left: 100, top: 100}, styles.genericCircle]}
-                />
-                <View
-                    ref={(circle) => {this.circle2 = circle;}}
-                    style={[{left: 200, top: 200}, styles.genericCircle]}
-                />
-                <View
-                    ref={(circle) => {this.circle3 = circle;}}
-                    style={[{left: 300, top: 300}, styles.genericCircle]}
-                />
-            {submenu}
+
+                {
+                    React.Children.map(
+                        this.props.children,
+                        function (child, i) {
+                            return React.cloneElement(
+                                child,
+                                {
+                                    layoutChanged: this._saveChildLayout.bind(this, i),
+                                    active: i == this.state.activeSubmenu
+                                });
+                        },
+                        this)
+                }
             </View>
         );
-
     },
 
-    _highlight: function() {
-        const circle = this.circle;
-        circle && circle.setNativeProps({
-            style: {
-                backgroundColor: processColor(CIRCLE_HIGHLIGHT_COLOR)
-            }
-        });
+    _onLayout: function(position) {
+        this._layout = position.nativeEvent.layout;
     },
 
-    _unHighlight: function() {
-        const circle = this.circle;
-        circle && circle.setNativeProps({
-            style: {backgroundColor: processColor(CIRCLE_COLOR)}
-        });
+    _saveChildLayout: function(childIndex, layout) {
+        this._childLayouts[childIndex] = layout;
     },
 
-    _updatePosition: function() {
-        this.circle && this.circle.setNativeProps(this._circleStyles);
+    _handlePanResponderGrant: function() {
+        //this.state.pan.setOffset(this._currentPan);
+        this.setState({dragging: true});
     },
 
     _handlePanResponderMove: function(e: Object, gestureState: Object) {
-        this._circleStyles.style.left = this._previousLeft + gestureState.dx;
-        this._circleStyles.style.top = this._previousTop + gestureState.dy;
-        this._updatePosition();
-        this._highlightOverlappingCircle(this._circleStyles.style);
+        this.state.pan.setValue({x: gestureState.dx, y: gestureState.dy});
+        this._highlightOverlappingCircle();
     },
 
     _handlePanResponderEnd: function(e: Object, gestureState: Object) {
-        this._unHighlight();
-        this._previousLeft += gestureState.dx;
-        this._previousTop += gestureState.dy;
-        // TODO: calculate again with new coordinates from this event.
-        if (this._hightlightedCircle) {
-            this._hightlightedCircle.setNativeProps({
-                style: { backgroundColor: processColor("black") }
-            });
-        }
+        this.setState({dragging: false});
+        this.state.pan.setValue({x: 0, y: 0});
+        this._highlightOverlappingCircle();
     },
 
-    _highlightOverlappingCircle: function(draggedCircleStyles) {
-        var circles = [
-            {ref: this.circle1, position: {left:100,top:100}},
-            {ref: this.circle2, position: {left:200,top:200}},
-            {ref: this.circle3, position: {left:300,top:300}}];
-        var overlappingCircle = circles.find(circle => this._hitTest(draggedCircleStyles, circle.position));
-        if (overlappingCircle == this._hightlightedCircle) {
-            return;
-        }
-
-        if (this._hightlightedCircle) {
-            this._hightlightedCircle.setNativeProps({
-                style: { backgroundColor: processColor("black") }
-            });
-        }
-        if (overlappingCircle) {
-            overlappingCircle.ref.setNativeProps({
-                style: { backgroundColor: processColor("yellow") }
-            });
-        }
-        this._hightlightedCircle = overlappingCircle ? overlappingCircle.ref : null;
-
-        this.setState({showSubmenu: overlappingCircle != null});
+    _highlightOverlappingCircle: function() {
+        var overlappingCircleIndex = this._childLayouts.findIndex(submenu => this._hitTest(submenu));
+        this.setState({activeSubmenu: overlappingCircleIndex});
     },
 
-    _hitTest: function(circle1, circle2) {
-        var circle1Center = {left: circle1.left + CIRCLE_SIZE/2, top: circle1.top + CIRCLE_SIZE/2};
-        var circle2Center = {left: circle2.left + CIRCLE_SIZE/2, top: circle2.top + CIRCLE_SIZE/2};
+    _hitTest: function(submenu) {
+        var circle1Center = {
+            x: this._currentPan.x + this._layout.x + this._layout.width / 2,
+            y: this._currentPan.y + this._layout.y + this._layout.height / 2
+        };
+        var circle2Center = {
+            x: submenu.x + submenu.width / 2,
+            y: submenu.y + submenu.height / 2
+        };
         var distanceBetweenCenters = Math.sqrt(
-            Math.pow(circle1Center.left - circle2Center.left, 2) +
-            Math.pow(circle1Center.top - circle2Center.top, 2));
-        return distanceBetweenCenters < CIRCLE_SIZE;
+            Math.pow(circle1Center.x - circle2Center.x, 2) +
+            Math.pow(circle1Center.y - circle2Center.y, 2));
+        var circle1Radius = this._layout.width / 2;
+        var circle2Radius = submenu.width / 2;
+        return distanceBetweenCenters < circle1Radius + circle2Radius;
+    },
+});
+
+var DragAndDropSubmenu = React.createClass({
+    render: function() {
+        var children = this.props.active ? this.props.children : null;
+        return (
+            <View>
+                <View
+                    style={[
+                        styles.submenu,
+                        this.props.style,
+                        this.props.active && styles.activeSubmenu]}
+                    onLayout={this.onLayout} />
+                {children}
+            </View>
+        );
+    },
+
+    onLayout: function(position) {
+        this.props.layoutChanged(position.nativeEvent.layout);
     }
 });
 
+var DragAndDropMenuItem = React.createClass({
+    getInitialState: function() {
+        return {
+            active: false
+        };
+    },
+
+    render: function() {
+        return (
+            <View {...this.props} style={styles.menuItem}/>
+        );
+    },
+});
 
 var styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#f5fcff'
     },
-    content: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    backButton: {
-        alignSelf: 'center',
-        marginBottom: 10,
-        borderRadius: 4,
-        borderWidth: 2,
-        borderColor: '#7a9e2b'
-    },
-    backText: {
-        backgroundColor: '#a9d947',
-        color: 'white',
-        padding: 5,
-        fontWeight: 'bold'
-    },
     circle: {
         width: CIRCLE_SIZE,
         height: CIRCLE_SIZE,
         borderRadius: CIRCLE_SIZE / 2,
-        backgroundColor: CIRCLE_COLOR,
+        backgroundColor: 'blue',
         position: 'absolute',
         left: 0,
         top: 0,
     },
-    genericCircle: {
+    draggingCircle: {
+        backgroundColor: 'red'
+    },
+    submenu: {
         width: CIRCLE_SIZE,
         height: CIRCLE_SIZE,
         borderRadius: CIRCLE_SIZE / 2,
         backgroundColor: 'black',
         position: 'absolute'
     },
-
+    activeSubmenu: {
+        backgroundColor: 'green'
+    },
+    menuItem: {
+        width: CIRCLE_SIZE,
+        height: CIRCLE_SIZE,
+        borderRadius: CIRCLE_SIZE / 2,
+        backgroundColor: 'black',
+        position: 'absolute'
+    },
 });
 
 module.exports = VerbGameScreen;
